3dda451635ffc18ec4bc98f1a0d8df3d3ab03ad8
avrahamferdman <aferdman@microsoft.com>
2026-04-06 20:43:46 +0300

    [development-developer-c6] developer work

--- packages/core/src/logging/index.ts
+++ packages/core/src/logging/index.ts
@@ -23,3 +23,23 @@ export type {
   LoggerConfig,
   LogTransport,
 } from './logger.js';
+
+// Performance Metrics
+export {
+  DEFAULT_MAX_METRICS,
+  MetricType,
+  PerformanceTimer,
+  PerformanceTracker,
+  _resetMetricIdCounter,
+} from './performance-tracker.js';
+
+export type {
+  MetricTokenUsage,
+  PerformanceMetric,
+  PerformanceMetricInput,
+  PerformanceReport,
+  PerformanceSummary,
+  PerformanceTrackerConfig,
+  TimerStartInput,
+  TimerStopInput,
+} from './performance-tracker.js';
